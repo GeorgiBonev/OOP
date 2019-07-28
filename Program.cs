a5e80dd106c0b4f6894e596d3cd525f2d057c1a4
GeorgiBonev <47987652+GeorgiBonev@users.noreply.github.com>
2019-07-28 11:44:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AnimalFeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var puppy = new Puppy();
+
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+            int integer = 500;
+
+            var cat = new Cat();
+            cat.Eat();
+
+            for (int i = integer; i > 0; i--)
+            {
+                Console.Beep(i, i);
+                i += 100;
+                Console.Beep(i, i);
+                if (i < 0)
+                {
+                    break;
+                }
+            }
+          
+
+        }
+    }
+}
